7e54f3b5b2d398eeeb753e3d30ff6ad1c8d6dc0b
Ulysse Carion <ulyssecarion@gmail.com>
2013-05-22 17:37:37 -0700

    Add special bitboard update method.

--- ext/onyx/board.c
+++ ext/onyx/board.c
@@ -33,3 +33,13 @@ Board* new_empty_board() {
 
     return board;
 }
+
+void update_special_bitboards(Board* board) {
+    board->white_pieces = 
+        board->white_pawns | board->white_knights | board->white_bishops | board->white_rooks | board->white_queens | board->white_king;
+
+    board->black_pieces = 
+        board->black_pawns | board->black_knights | board->black_bishops | board->black_rooks | board->black_queens | board->black_king;
+
+    board->all_pieces = board->white_pieces | board->black_pieces;
+}
