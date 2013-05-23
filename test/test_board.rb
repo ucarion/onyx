d17dba2fa1a79c989a5fe4a70d991bed1d4bf1fb
Ulysse Carion <ulyssecarion@gmail.com>
2013-05-22 17:10:15 -0700

    Add new testing for board's to_s method.

--- test/test_board.rb
+++ test/test_board.rb
@@ -1,9 +1,44 @@
 require_relative 'test_helper'
 
 class MoveTest < Onyx::TestCase
+  def setup
+    @board_to_s = <<-BOARD
+     1   2   3   4   5   6   7   8  
+   +---+---+---+---+---+---+---+---+
+ h | r | n | b | q | k | b | n | r | h 
+   +---+---+---+---+---+---+---+---+
+ g | p | p | p | p | p | p | p | p | g 
+   +---+---+---+---+---+---+---+---+
+ f |   |   |   |   |   |   |   |   | f 
+   +---+---+---+---+---+---+---+---+
+ e |   |   |   |   |   |   |   |   | e 
+   +---+---+---+---+---+---+---+---+
+ d |   |   |   |   |   |   |   |   | d 
+   +---+---+---+---+---+---+---+---+
+ c |   |   |   |   |   |   |   |   | c 
+   +---+---+---+---+---+---+---+---+
+ b | P | P | P | P | P | P | P | P | b 
+   +---+---+---+---+---+---+---+---+
+ a | R | N | B | Q | K | B | N | R | a 
+   +---+---+---+---+---+---+---+---+
+     1   2   3   4   5   6   7   8  
+
+White to move.
+White can castle: O-O O-O-O
+Black can castle: O-O O-O-O
+En passant loc: -1
+Fifty move rule: 0
+BOARD
+  end
+
   def test_can_create_board
     assert_nothing_raised do
       Onyx::Board.new
     end
+  end
+
+  def test_can_read_from_fen
+    board = Onyx::Board.new_from_fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+    assert_equal(@board_to_s, board.to_s)
   end
 end
