934eaf566820a5d235965f03cbf97cde90eeaca7
Ulysse Carion <ulyssecarion@gmail.com>
2013-05-24 14:01:08 -0700

    Add undo_move to board.
    Testing suites need to be developed more for this; hopefully perft will
    definitively prove this program's effectiveness.

--- ext/onyx/board.h
+++ ext/onyx/board.h
@@ -9,7 +9,22 @@ typedef unsigned long long Bitboard;
 #include "move.h"
 #include "utils.h"
 
+#define MAX_GAME_PLY 1024 // maximum game length
+#define MAX_TOTAL_MOVES 4096 // maximum available moves in a position
+
 typedef struct {
+    char fifty_move_rule;
+    char en_passant_loc;
+    bool white_castle_k;
+    bool white_castle_q;
+    bool black_castle_k;
+    bool black_castle_q;
+} BoardRecord;
+
+typedef struct {
+    Move movelist[MAX_GAME_PLY];
+    BoardRecord history[MAX_GAME_PLY];
+
     Bitboard white_pawns;
     Bitboard white_knights;
     Bitboard white_bishops;
@@ -28,6 +43,12 @@ typedef struct {
     Bitboard black_pieces;
     Bitboard all_pieces;
 
+    // move_number is our current ply count in the game
+    // end_of_search is the deepest ply reached while searching
+    // end_of_search - move_number is how far into the future we've searched.
+    int move_number;
+    int end_of_search;
+
     char fifty_move_rule;
     char en_passant_loc;
 
@@ -42,5 +63,9 @@ typedef struct {
 Board* new_empty_board();
 void update_special_bitboards(Board* board);
 void make_move(Board* board, Move move);
+void unmake_move(Board* board, Move move);
+void unmake_capture(Board* board, Bitboard sq, char piece);
+void unmake_white_promotion(Board* board, Bitboard to, char flag);
+void unmake_black_promotion(Board* board, Bitboard to, char flag);
 
 #endif
