b041ee36d17d75d0217720f048361342c630e942
Ulysse Carion <ulyssecarion@gmail.com>
2013-05-23 18:47:51 -0700

    Remove move's now defunct test for instantiation.

--- test/test_move.rb
+++ test/test_move.rb
@@ -1,9 +1,5 @@
 require_relative 'test_helper'
 
 class MoveTest < Onyx::TestCase
-  def test_cannot_call_move_new
-    assert_raise(NoMethodError) do
-      Onyx::Move.new(0, 0, 0, false, 0)
-    end
-  end
+
 end
