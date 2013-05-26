9ce36b6af835e96f78580f43941ec28ed9ac1223
Ulysse Carion <ulyssecarion@gmail.com>
2013-05-26 10:38:23 -0700

    Switch over to new magic bitboard generation
    Get rid of my mask generators, and add a call to initmagicmoves().

--- ext/onyx/movegen.h
+++ ext/onyx/movegen.h
@@ -3,19 +3,13 @@
 
 #include "board.h"
 #include "utils.h"
+#include "magicmoves.h"
 
 extern Bitboard king_movelocs[64];
 extern Bitboard knight_movelocs[64];
-extern Bitboard rank_masks[64];
-extern Bitboard file_masks[64];
-extern Bitboard a1h8_masks[64];
-extern Bitboard a8h1_masks[64];
 
 void movegen_init(void);
 void init_movegen_kings(void);
 void init_movegen_knights(void);
-void init_movegen_rook_masks(void);
-void init_movegen_a1h8_masks(void);
-void init_movegen_a8h1_masks(void);
 
 #endif
