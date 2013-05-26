4796e07e979d9f8b95a4a30ddae68d19d0fcf9aa
Ulysse Carion <ulyssecarion@gmail.com>
2013-05-25 19:39:46 -0700

    Use to_bitboard instead of bitwise operations in knight_movelocs
    Bitwise is hard to read.

--- ext/onyx/movegen.c
+++ ext/onyx/movegen.c
@@ -60,30 +60,30 @@ void init_movegen_knights(void) {
 
         if (file > 0) {
             if (rank > 1)
-                knight_movelocs[i] |= 1ULL << ((rank - 2) * 8 + file - 1);
+                knight_movelocs[i] |= to_bitboard(rank - 2, file - 1);
             if (rank < 6)
-                knight_movelocs[i] |= 1ULL << ((rank + 2) * 8 + file - 1);
+                knight_movelocs[i] |= to_bitboard(rank + 2, file - 1);
         }
 
         if (file > 1) {
             if (rank > 0)
-                knight_movelocs[i] |= 1ULL << ((rank - 1) * 8 + file - 2);
+                knight_movelocs[i] |= to_bitboard(rank - 1, file - 2);
             if (rank < 7)
-                knight_movelocs[i] |= 1ULL << ((rank + 1) * 8 + file - 2);
+                knight_movelocs[i] |= to_bitboard(rank + 1, file - 2);
         }
 
         if (file < 6) {
             if (rank > 0)
-                knight_movelocs[i] |= 1ULL << ((rank - 1) * 8 + file + 2);
+                knight_movelocs[i] |= to_bitboard(rank - 1, file + 2);
             if (rank < 7)
-                knight_movelocs[i] |= 1ULL << ((rank + 1) * 8 + file + 2);
+                knight_movelocs[i] |= to_bitboard(rank + 1, file + 2);
         }
 
         if (file < 7) {
             if (rank > 1)
-                knight_movelocs[i] |= 1ULL << ((rank - 2) * 8 + file + 1);
+                knight_movelocs[i] |= to_bitboard(rank - 2, file + 1);
             if (rank < 6)
-                knight_movelocs[i] |= 1ULL << ((rank + 2) * 8 + file + 1);
+                knight_movelocs[i] |= to_bitboard(rank + 2, file + 1);
         }
     }
 }
